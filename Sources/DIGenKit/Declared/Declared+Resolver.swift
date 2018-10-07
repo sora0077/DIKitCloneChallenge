6d086e605b56bca229a07fcd6b825abd81d3774f
sora0077 <t.hayashi0077+github@gmail.com>
2018-10-08 03:21:04 +0900

    Add ResolverTests

--- Sources/DIGenKit/Declared/Declared+Resolver.swift
+++ Sources/DIGenKit/Declared/Declared+Resolver.swift
@@ -5,17 +5,28 @@
 //  Created by 林達也 on 2018/10/06.
 //
 
+import Foundation
 import SwiftSyntax
 
 extension Declared {
 
     struct Resolver {
         let providedFunctions: [ProvidedFunction]
-
         let decl: ProtocolDeclSyntax
 
         init?(decl: ProtocolDeclSyntax) throws {
-            return nil
+            let found = decl.helper.inheritanceClause?.inheritedTypeCollection.contains(where: {
+                $0.typeName.helper.name?.text == "Resolver"
+            }) ?? false
+
+            if !found {
+                return nil
+            }
+
+            self.providedFunctions = try decl.members.members
+                .compactMap { $0 as? FunctionDeclSyntax }
+                .compactMap(ProvidedFunction.init(decl:))
+            self.decl = decl
         }
     }
 }
@@ -24,17 +35,54 @@ extension Declared.Resolver {
 
     struct ProvidedFunction {
 
-        let argumentTypes: [Declared.SwiftType]
+        struct Error: LocalizedError {
+            enum Reason {
+                case returnTypeNotFound
+                case nonInstanceMethod
+                case nonMethod
+            }
+
+            let decl: DeclSyntax
+            let reason: Reason
+
+            var errorDescription: String? {
+                switch reason {
+                case .returnTypeNotFound:
+                    return "Provide method must return non-void type"
+                case .nonInstanceMethod:
+                    return "Provide method must not be static"
+                case .nonMethod:
+                    return "Provide method must not be an initalizer"
+                }
+            }
+        }
+
+        let methodName: String
+        let arguments: [(label: String, type: Declared.SwiftType)]
         let returnType: Declared.SwiftType
 
         let decl: FunctionDeclSyntax
 
         init?(decl: FunctionDeclSyntax) throws {
-            return nil
+            guard decl.helper.identifier?.text.starts(with: "provide") ?? false else {
+                return nil
+            }
+
+            guard let returnType = Declared.SwiftType(decl.signature.output?.returnType), returnType.name != "Void" else {
+                throw Error(decl: decl, reason: .returnTypeNotFound)
+            }
+
+            guard !decl.helper.isStatic else {
+                throw Error(decl: decl, reason: .nonInstanceMethod)
+            }
+
+            self.decl = decl
+            self.methodName = decl.identifier.text
+            self.returnType = returnType
+            self.arguments = decl.signature.input.parameterList.compactMap {
+                guard let label = $0.firstName?.text, let type = Declared.SwiftType($0.type) else { return nil }
+                return (label, type)
+            }
         }
-    }
-
-    struct ResolvedFunction {
-
     }
 }
---
+++ Tests/DIGenKitTests/Declared/ResolverTests.swift
@@ -0,0 +1,106 @@
+//
+//  ResolverTests.swift
+//  DIGenKit
+//
+//  Created by 林達也 on 2018/10/08.
+//
+
+import XCTest
+@testable import DIGenKit
+
+import SwiftSyntax
+
+private class Collector: SyntaxVisitor {
+    var nodes = [Declared.Resolver]()
+    var errors = [Error]()
+
+    override func visit(_ node: ProtocolDeclSyntax) {
+        super.visit(node)
+        do {
+            if let resolver = try Declared.Resolver(decl: node) {
+                nodes.append(resolver)
+            }
+        } catch {
+            errors.append(error)
+        }
+    }
+}
+
+class ResolverTests: XCTestCase {
+    func test() throws {
+
+        let tempDir = try temporaryDirectory()
+        let collector = Collector()
+        try collector.visit(makeSourceSyntax("Foo.swift", in: tempDir) {
+            """
+            protocol Test: Resolver {
+                func provideA(b: B, c: C?) -> A
+            }
+            """
+        })
+
+        guard let resolver = collector.nodes.first else {
+            XCTFail()
+            return
+        }
+
+        XCTAssertEqual(resolver.providedFunctions.count, 1)
+        XCTAssertEqual(resolver.providedFunctions[0].methodName, "provideA")
+        XCTAssertEqual(resolver.providedFunctions[0].arguments.map { $1.name }, ["B", "C?"])
+        XCTAssertEqual(resolver.providedFunctions[0].returnType.name, "A")
+    }
+
+    func testMissingReturnType() throws {
+        let tempDir = try temporaryDirectory()
+        let collector = Collector()
+        try collector.visit(makeSourceSyntax("Foo.swift", in: tempDir) {
+            """
+            protocol Test: Resolver {
+                func provideA(b: B, c: C)
+            }
+            """
+        })
+
+        do {
+            XCTAssertEqual(collector.errors.count, 1)
+            if let error = collector.errors.first {
+                throw error
+            }
+            XCTFail()
+        } catch let error as Declared.Resolver.ProvidedFunction.Error {
+            XCTAssertEqual(error.reason, .returnTypeNotFound)
+        } catch {
+            XCTFail()
+        }
+    }
+
+    func testStatic() throws {
+        let tempDir = try temporaryDirectory()
+        let collector = Collector()
+        try collector.visit(makeSourceSyntax("Foo.swift", in: tempDir) {
+            """
+            protocol Test: Resolver {
+                static func provideA(b: B, c: C) -> A
+            }
+            """
+        })
+
+        do {
+            XCTAssertEqual(collector.errors.count, 1)
+            if let error = collector.errors.first {
+                throw error
+            }
+            XCTFail()
+        } catch let error as Declared.Resolver.ProvidedFunction.Error {
+            XCTAssertEqual(error.reason, .nonInstanceMethod)
+        } catch {
+            XCTFail()
+        }
+    }
+
+    static var allTests = [
+        ("test", test),
+        ("testMissingReturnType", testMissingReturnType),
+        ("testStatic", testStatic)
+    ]
+}
